95b3c464a15acd7a117c8f5fe13fc81c8a41dbeb
ininkevin <87509103+ininkevin@users.noreply.github.com>
2022-12-19 17:37:01 +0800

    Update download.sh

--- tools/download.sh
+++ tools/download.sh
@@ -11,12 +11,6 @@ wget -P data https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_test_annotations.
 wget -P data https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_questions.json
 wget -P data https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_test_questions.json
 
-# VQA-CP1
-wget -P data https://computing.ece.vt.edu/~aish/vqacp/vqacp_v1_train_annotations.json
-wget -P data https://computing.ece.vt.edu/~aish/vqacp/vqacp_v1_test_annotations.json
-wget -P data https://computing.ece.vt.edu/~aish/vqacp/vqacp_v1_train_questions.json
-wget -P data https://computing.ece.vt.edu/~aish/vqacp/vqacp_v1_test_questions.json
-
 # VQA-V2
 wget -P data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
 unzip data/v2_Questions_Train_mscoco.zip -d data
